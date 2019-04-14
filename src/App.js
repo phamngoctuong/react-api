92a64c4b0502783efcac2f4198b4da1e3d5886b3
Pham Ngoc Tuong <phamngoctuong1805@gmail.com>
2019-04-15 01:00:33 +0700

    Initial Project

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
-import Menu from './components/Menu/Menu';
-import routes from './routes';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-
 class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Menu />
-                    <div className="container">
-                        <div className="row">
-                            {this.showContentMenus(routes)}
-                        </div>
-                    </div>
-                </div>
-            </Router>
-        );
-    }
-
-    showContentMenus = (routes) => {
-        var result = null;
-        if (routes.length > 0) {
-            result = routes.map((route, index) => {
-                return (
-                    <Route
-                        key={index}
-                        path={route.path}
-                        exact={route.exact}
-                        component={route.main}
-                    />
-                );
-            });
-        }
-        return <Switch>{result}</Switch>;
-    }
-
+  render() {
+    return (
+      <div className="App">
+        App
+      </div>
+    );
+  }
 }
 
 export default App;
