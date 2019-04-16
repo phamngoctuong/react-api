092cc98a7b170c73bbfaadbef37f86b0ca854cf6
Pham Ngoc Tuong <phamngoctuong1805@gmail.com>
2019-04-17 00:43:56 +0700

    Tach Axios xu ly p1

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 import ProductList from './../../components/ProductList/ProductList';
 import ProductItem from './../../components/ProductItem/ProductItem';
 import {connect} from 'react-redux';
+import axios from 'axios';
 class ProductListPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: []
+        }
+    }
+    componentDidMount(){
+        axios({
+            method: 'get',
+            url: 'http://localhost:3000/products'
+        }).then(respon=>{
+            this.setState({
+                products : respon.data
+            })
+        }).catch(error=>{
+            console.log(error);
+        });
+    }
     showProducts = (products) => {
         var result = null;
         if(products.length > 0){
@@ -20,7 +39,8 @@ class ProductListPage extends Component {
         return result;
     }
     render() {
-        var {products} = this.props;
+        var {products} = this.state;
+        console.log(products);
         return (
            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <button type="button" className="btn btn-info mb-10">
